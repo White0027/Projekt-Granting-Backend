00589be4b5d68b1aec4513a013fdd67ed660be1a
White 002 <116343286+White0027@users.noreply.github.com>
2025-01-27 21:24:54 +0000

    update

--- tests/gt/gt_utils.py
+++ tests/gt/gt_utils.py
@@ -65,8 +65,8 @@ import re
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print("dir_path", dir_path, flush=True)
 
-location = "./src/tests/gqls"
-location = re.sub(r"\\tests\\.+", r"\\tests\\gqls", dir_path)
+location = "./tests/gql"
+location = re.sub(r"\\tests\\.+", r"\\tests\\gql", dir_path)
 # location
 print("location", location, flush=True)
 logging.info(f"Queries location {dir_path} => {location}")
